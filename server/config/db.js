038aa997d05d9dba4923dd6dfba46c0db34e165d
Vincenzo Uccello <vincenzo.uc21@gmail.com>
2025-04-08 18:29:53 +0200

    set up file .env

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
-
-const uri = "mongodb+srv://avis:8LYyTauhpLyVp8JI@studyupcluster.hytamy1.mongodb.net/?retryWrites=true&w=majority&appName=StudyUpCluster"
+import dotenv from "dotenv";
+dotenv.config();
+const uri = process.env.MONGODB_URI;
   
 const client = new MongoClient(uri);
 
