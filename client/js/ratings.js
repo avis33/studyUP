34f645a3d783825120b2e2caf1dfe63f416359a1
Vincenzo Uccello <vincenzo.uc21@gmail.com>
2025-04-30 10:25:28 +0200

    Aggiunta dashboard personalizzata per studenti e tutor

---
+++ client/js/ratings.js
@@ -0,0 +1,42 @@
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const token = localStorage.getItem("authToken");
+    const loginButton = document.getElementById("openModalBtn");
+  
+    if (!token) {
+      document.getElementById("user-area").style.display = "none";
+      return;
+    }
+    try {
+      // chiamata a server per verificare se esiste il token
+      const res = await fetch("http://localhost:3000/user/profilo", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // il middleware sul server prende il token da qua
+        },
+      });
+      const data = await res.json();
+      if (data.hasAccess) {
+        //logica per quando l'utente è loggato correttamente
+        document.getElementById("user-area").classList.remove("hidden");
+        document.getElementById("openModalBtn").classList.add("hidden");
+        document.getElementById("userName").innerText =
+          data.user.firstName.length > 20
+            ? data.user.firstName.substring(0, 20) + "..." //tronca dopo 20 caratteri
+            : data.user.firstName;
+        document.getElementById("userProfile").src =
+          data.user?.profilePicture || "../assets/icons/icone_img.svg";
+          if(data.user.role == "student"){
+            document.getElementById("dashboard").innerText = "I miei tutor"
+          }else{
+            document.getElementById("dashboard").innerText = "I miei studenti"
+          }
+      }
+    } catch (error) {
+      document.getElementById("user-area").classList.add("hidden");
+      loginButton.style.display = "none";
+      console.error("Errore:", error);
+    }
+ 
+  });
