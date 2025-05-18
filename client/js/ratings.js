acca917e2883da551c6e42e6d8edc49e7d946c56
Vincenzo Uccello <vincenzo.uc21@gmail.com>
2025-05-18 15:05:24 +0200

    aggiustato link

--- client/js/ratings.js
+++ client/js/ratings.js
@@ -1,46 +1,4 @@
-function generateStars(rating) {
-  const fullStars = Math.floor(rating);
-  const halfStar = rating % 1 >= 0.25 && rating % 1 < 0.75;
-  const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
-  let html = "";
-  for (let i = 0; i < fullStars; i++) {
-    html += `<span class="star full">&#9733;</span>`;
-  }
-  if (halfStar) {
-    html += `<span class="star half">&#9733;</span>`;
-  }
-  for (let i = 0; i < emptyStars; i++) {
-    html += `<span class="star empty">&#9733;</span>`;
-  }
-  return html;
-}
-// Funzione per generare avatar colorati basati sulle iniziali
-function getAvatarColor(name) {
-  const colors = [
-    "#4361ee",
-    "#3f37c9",
-    "#4895ef",
-    "#4cc9f0",
-    "#4895ef",
-    "#3a0ca3",
-    "#7209b7",
-    "#b5179e",
-    "#f72585",
-    "#560bad",
-    "#480ca8",
-    "#3a0ca3",
-  ];
-  const initials = name
-    .split(" ")
-    .map((n) => n[0])
-    .join("")
-    .toUpperCase();
-  const colorIndex = initials.charCodeAt(0) % colors.length;
-  return {
-    initials: initials.substring(0, 2),
-    color: colors[colorIndex],
-  };
-}
+import { getAvatarColor,generateStars } from "./utils/utils.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const token = localStorage.getItem("authToken");
